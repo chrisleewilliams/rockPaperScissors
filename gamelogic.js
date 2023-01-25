78d56d944c54cf416af59a290ab89a1aa46bddca
Chris Williams <chrisleewilliams.84@gmail.com>
2023-01-24 21:09:48 -0600

    final changes to the rock paper scissor game

--- gamelogic.js
+++ gamelogic.js
@@ -15,26 +15,37 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("You tied this round");
+    return 0;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log("You won this round, gret job!");
+    return 1;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     console.log("You won this round, gret job!");
+    return 1;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("You won this round, gret job!");
+    return 1;
   } else {
-    console.log("You lost, better luck next time.");
+    console.log("You lost this round, better luck next time.");
+    return -1;
   }
 }
 
 //play 5 rounds and keep track of the score then report a winner
 function game() {
-  player = 0;
-  computer = 0;
+  playerScore = 0;
   for (let i = 0; i < 5; i++) {
     let userChoice = prompt("Choose rock, paper or scissors at your own risk!");
 
     //makes the playerSelection case insensitive
     let lowerCaseUserChoice = userChoice.toLowerCase();
-    playRound(lowerCaseUserChoice, getComputerChoice());
+
+    //logic to keep track of score
+    playerScore += playRound(lowerCaseUserChoice, getComputerChoice());
   }
+  if (playerScore < 0) {
+    console.log("You lost. Would you like to try again?");
+  } else if (player === 0) {
+    console.log("You tied this time. Good Game!");
+  } else console.log("YOU WON!!!");
 }
